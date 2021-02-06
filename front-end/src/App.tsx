d4e0f45d28b04ae04de331966c5d9cc9effe1a04
ahmednkhan24 <ahmednkhan24@gmail.com>
2021-02-05 23:30:28 -0600

    added routing between 2 pages

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import Content from './components/Content';
 
 const App: React.FC = () => {
   return (
-    <div>
-      <h1>hello world</h1>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <div className="container">
+        <Route path="/" exact component={Home} />
+        <Route path="/content" exact component={Content} />
+      </div>
+    </BrowserRouter>
   );
 };
 
